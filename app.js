6ee35529d1162aef99e6393ee236d2dc6f8f3e30
Ayushman <avenuedesign768@gmail.com>
2026-08-11 20:57:27 +0530

    class 01

---
+++ app.js
@@ -0,0 +1,52 @@
+// alert("Hello Javascript");
+// console.log("Hello  TCM"); - Rec
+// document.write(2 + 3);
+// confirm("Are you want to delete this ?");
+// prompt("Please enter your product QTY.");
+
+// var (Old), let, const
+
+// --var = keyword, name = variable, = = operator , Ayushman = Value
+// var name = "Ayushman";
+
+// Declearation
+// var name;
+
+// // Initilization/ assign
+// name = "Ayushman";
+// // Initilization/ assign
+// name = "Alok";
+// // Initilization/ assign
+// name = "Rahul";
+// // Initilization/ assign
+// name = "Manoj";
+// // Initilization/ assign
+// name = "Rajesh";
+
+// // Declearation
+// var name;
+
+// // Initilization/ assign
+// name = "Sumit";
+
+// console.log(name);
+
+
+// Declearation
+// let name;
+
+// // Initilization/ assign
+// name = "Ayushman";
+
+// // Initilization/ assign
+// name = "Rakesh";
+
+// // Initilization/ assign
+// name = "Ranjit";
+
+// console.log(name)
+
+
+
+// const roll = 8238278;
+// console.log(roll)
